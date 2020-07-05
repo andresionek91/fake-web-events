cbcac2de3f6f7853171a522c51dc67803f98da71
Andre Sionek <andresionek91@gmail.com>
2020-07-05 14:27:29 +0100

    add assets and examples

---
+++ examples/print_events.py
@@ -0,0 +1,11 @@
+from fake_web_events.user import UserPool
+from fake_web_events.simulation import Simulation, simulate_events
+
+
+user_pool = UserPool(size=200)
+simulation = Simulation(user_pool=user_pool, sessions_per_day=100000)
+events = simulate_events(simulation, duration_seconds=60)
+
+
+for event in events:
+    print(event)
