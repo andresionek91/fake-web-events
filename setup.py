2baf3d4af07c0985c34f9433ee5a629caaa9b2ff
Andre Sionek <andresionek91@gmail.com>
2020-07-05 15:06:09 +0100

    add long description

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
 from setuptools import setup
 
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
 setup(
     name='fake_web_events',
     version='0.1.0',
     description='Generator of semi-random fake web events. ',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     keywords=['fake', 'web events', 'events'],
     license='MIT',
     author='Andre Sionek',
