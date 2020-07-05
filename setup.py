ae7f8ee62b6127cf66abddfa0f28733394c1872b
Andre Sionek <andresionek91@gmail.com>
2020-07-05 15:14:13 +0100

    update version

--- setup.py
+++ setup.py
@@ -6,10 +6,9 @@ this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-
 setup(
     name='fake_web_events',
-    version='0.1.0',
+    version='0.1.1',
     description='Generator of semi-random fake web events. ',
     long_description=long_description,
     long_description_content_type='text/markdown',
@@ -18,9 +17,6 @@ setup(
     author='Andre Sionek',
     author_email='andresionek91@gmail.com',
     url="https://github.com/andresionek91/fake-web-events",
-    project_urls={
-        "Documentation": "https://github.com/andresionek91/fake-web-events/blob/master/README.md"
-    },
     packages=['fake_web_events'],
     install_requires=['pyaml==20.4.0', 'pytest==5.4.3', 'faker==4.1.1'],
     classifiers=[
